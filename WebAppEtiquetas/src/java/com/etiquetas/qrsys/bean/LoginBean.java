a9d4a4a45cf609877d46db052aa21c35f9648fd5
Mario Arias Gómez <msc.mario.sistemas@gmail.com>
2022-11-19 19:10:00 -0600

    Seguridad SSL

--- WebAppEtiquetas/src/java/com/etiquetas/qrsys/bean/LoginBean.java
+++ WebAppEtiquetas/src/java/com/etiquetas/qrsys/bean/LoginBean.java
@@ -20,40 +20,41 @@ import javax.inject.Named;
 @Named(value = "loginBean")
 @SessionScoped
 public class LoginBean implements Serializable {
-
+    
     private static final long serialVersionUID = 1L;
     private Usuario usuario;
     private String nombre;
     private String password;
-
+    private final String llave = "58ef111e55611ac385c35fc7ca6ae5fd290111cac2286bd5fb023ae96aa48c2f47c98ae894cb11ef7f72eb20f843fd13f814cad94757b86509c274f0a040aca8";
+    
     public Usuario getUsuario() {
         return usuario;
     }
-
+    
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
-
+    
     public String getNombre() {
         return nombre;
     }
-
+    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    
     public String getPassword() {
         return password;
     }
-
+    
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
     public LoginBean() {
         this.usuario = new Usuario();
     }
-
+    
     public void login() throws IOException {
         RequestContext context = RequestContext.getCurrentInstance();
         @SuppressWarnings("UnusedAssignment")
@@ -61,37 +62,38 @@ public class LoginBean implements Serializable {
         @SuppressWarnings("UnusedAssignment")
         boolean loggedIn = false;
         String ruta = "";
-
+        
         UsuarioDao uDao = new UsuarioDaoImp();
+        this.usuario.setPassword(llave + this.usuario.getPassword());
         this.usuario = uDao.login(this.usuario);
-
+        
         if (this.usuario != null) {
             loggedIn = true;
             FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuario", this.usuario);
             message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenido", this.usuario.getNombre());
             switch (this.usuario.getPerfil().getIdperfil()) {
                 case 1:
-                    ruta = "/Views/Home.jsf";
-                    //ruta = "/WebAppEtiquetas/Views/Home.jsf";
+                    //ruta = "/Views/Home.jsf";
+                    ruta = "/WebAppEtiquetas/Views/Home.jsf";
                     break;
                 case 2:
-                    ruta = "/Views/Sae.jsf";
-                    //ruta = "/WebAppEtiquetas/Views/Sae.jsf";
+                    //ruta = "/Views/Sae.jsf";
+                    ruta = "/WebAppEtiquetas/Views/Sae.jsf";
                     break;
                 default:
-                    ruta = "/Views/Home.jsf";
-                    //ruta = "/WebAppEtiquetas/Views/Home.jsf";
+                    //ruta = "/Views/Home.jsf";
+                    ruta = "/WebAppEtiquetas/Views/Home.jsf";
                     break;
             }
-
+            
         } else {
             loggedIn = false;
-
+            
             message = new FacesMessage(FacesMessage.SEVERITY_WARN, "¡Error de sesión!",
                     "Usuario o password incorrectos...");
             FacesContext.getCurrentInstance().addMessage(null, message);
             this.usuario = new Usuario();
-
+            
         }
         FacesContext.getCurrentInstance().addMessage(null, message);
         context.addCallbackParam("loggedIn", loggedIn);
@@ -99,9 +101,9 @@ public class LoginBean implements Serializable {
         con.redirect(ruta);
         //context.addCallbackParam("ruta", ruta);
     }
-
+    
     public void cerrarSesion() {
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
     }
-
+    
 }
--- WebAppEtiquetas/src/java/com/etiquetas/qrsys/dao/Conexion.java
+++ WebAppEtiquetas/src/java/com/etiquetas/qrsys/dao/Conexion.java
@@ -6,27 +6,20 @@ import java.sql.SQLException;
 
 public class Conexion {
 
-//    private final String URL = "jdbc:sqlserver://10.130.22.211\\SQL2014;databaseName=SAE80Empre01";
-//    private final String USERNAME = "sa";
-//    private final String PASS = "aspel$2020";
-//    private final String USERNAME2 = "gastos_usrdb";
-//    private final String PASS2 = "k1CruJ@raDix";
-//    private final String URL2 = "jdbc:sqlserver://10.220.221.158\\SQLEXPRESS;databaseName=dbqrsys";
-    
-    
-//    private final String URL = "jdbc:sqlserver://ASPEL;databaseName=SAE70Empre01";
-//    private final String USERNAME = "gastos";
-//    private final String USERNAME2 = "gastos";
-//    private final String PASS = "k1CruJ@raDix";
-//    private final String PASS2 = "k1CruJ@raDix";
-//    private final String URL2 = "jdbc:sqlserver://ASPEL;databaseName=dbqrsys";
-    
-    private final String URL = "jdbc:sqlserver://DESKTOP-557O6CE\\SQLEXPRESS;databaseName=SAE80Empre01";
+    private final String URL = "jdbc:sqlserver://10.130.22.211\\SQL2014;databaseName=SAE80Empre01";
     private final String USERNAME = "sa";
-    private final String USERNAME2 = "sa";
-    private final String PASS = "dmsistemas";
-    private final String PASS2 = "dmsistemas";
-    private final String URL2 = "jdbc:sqlserver://DESKTOP-557O6CE\\SQLEXPRESS;databaseName=dbqrsys";
+    private final String PASS = "aspel$2020";
+    private final String USERNAME2 = "gastos_usrdb";
+    private final String PASS2 = "k1CruJ@raDix";
+    private final String URL2 = "jdbc:sqlserver://10.220.221.158\\SQLEXPRESS;databaseName=dbqrsys";
+    
+   
+//    private final String URL = "jdbc:sqlserver://DESKTOP-9TOA5T5\\SQLEXPRESS;databaseName=SAE80Empre01";
+//    private final String USERNAME = "sa";
+//    private final String USERNAME2 = "sa";
+//    private final String PASS = "dev22";
+//    private final String PASS2 = "dev22";
+//    private final String URL2 = "jdbc:sqlserver://DESKTOP-9TOA5T5\\SQLEXPRESS;databaseName=dbqrsys";
 
     private Connection cn;
     private Connection cnqr;
